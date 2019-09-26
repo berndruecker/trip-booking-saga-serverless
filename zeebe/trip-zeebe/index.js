093df466787d5298de70aae4003c7e4ea6b6bcdb
Bernd Rücker <bernd.ruecker@camunda.com>
2019-09-26 14:30:07 +0200

    switched from test to prod environment

--- zeebe/trip-zeebe/index.js
+++ zeebe/trip-zeebe/index.js
@@ -6,21 +6,12 @@ module.exports.handler = (input, context, callback) => {
   const body = JSON.parse(input.body);
 
   const zbc = new ZB.ZBClient({
-//    camundaCloud: {
-//      clientId: "KEFgrt9Azh07wTXbTX89sIBPgwgvzGaH",
-//      clientSecret:
-//      "vRfUxMcwuqYg4om6HYweR8iJdZyBl89BS9a5Bi4W7nOV5ezcvfOz4N4uO2vKAyWx",
-//      clusterId: "2daa1cac-f7cf-45e7-98f8-cccb474bf329",
-//      cacheOnDisk: false      
-//    },
-    hostname: "2daa1cac-f7cf-45e7-98f8-cccb474bf329.zeebe.ultrawombat.com",
-    port: "443",
-    oAuth: {
-        url: "https://login.cloud.ultrawombat.com/oauth/token",
-        audience: "2daa1cac-f7cf-45e7-98f8-cccb474bf329.zeebe.ultrawombat.com",
-        clientId: "KEFgrt9Azh07wTXbTX89sIBPgwgvzGaH",
-        clientSecret: "vRfUxMcwuqYg4om6HYweR8iJdZyBl89BS9a5Bi4W7nOV5ezcvfOz4N4uO2vKAyWx",
-        cacheOnDisk: false
+    camundaCloud: {
+      clientId: "KEFgrt9Azh07wTXbTX89sIBPgwgvzGaH",
+      clientSecret:
+      "vRfUxMcwuqYg4om6HYweR8iJdZyBl89BS9a5Bi4W7nOV5ezcvfOz4N4uO2vKAyWx",
+      clusterId: "2daa1cac-f7cf-45e7-98f8-cccb474bf329",
+      cacheOnDisk: false      
     }
   })
 
